dbfda65b2874b88d09011c8cbe9b6e5275ecc2e0
Wiktor Jerzykiewicz <wiktor.jerzykiewicz@uczen.zsk.poznan.pl>
2025-02-23 17:12:39 +0100

    Created base of the project

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CATALOGUE</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div id="logo"><span>C</span>atalogue</div>
+        <nav>
+            <a href="" class="linknav">Zacznij tworzyć</a>
+                
+            <a href="" class="linknav">Panel projektów</a>
+
+            <a href="" class="linknav">User</a>
+        </nav>
+        
+    </header>
+    <main>
+        <section class="row-1">
+            <h1>Czym jest Catalogue?</h1>
+            <article>
+                Catalogue to interaktywne narzędzie, które umożliwia tworzenie własnych stron.
+                Strony stworzone za pomocą Catalogue, pozwalają na przejżyste przechowywanie plików, linków i wielu innych rzeczy.
+
+            </article>
+        </section>
+
+        <section class="row-1">
+            <h1>Pierwsze kroki</h1>
+            <article>
+                Obsługa programu jest prosta. Kreator znajdujący się pod zakładką "Zacznij tworzyć" w intuicyjny sposób pozwala szybko i sprawnie stworzyć twój własny katalog.
+
+            </article>
+        </section>
+
+        <section class="row-1">
+            <h1>Wygoda</h1>
+            <article>
+                Twoje wszystkie stronki są przechowywane u nas. Gdy stwierdzisz że wszystko jest już gotowe, wystarczy że klikniesz w panel projektów i wyeksportujesz swój katalog.
+            </article>
+        </section>
+        <section class="row-2">
+            <h1>Szablony</h1>
+            <article>Zanim zaczniesz tworzyć, sprawdź który szablon pasuje najlepiej dla twojego katalogu:</article>
+            <article>
+                <!-- Szabloniki dodawane z bazki-->
+            </article>
+        </section>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
